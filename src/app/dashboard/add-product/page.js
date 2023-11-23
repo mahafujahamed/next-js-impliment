e7d7450600e3b0eda3f0ac16de81400e1749f85f
Mahafuj-Ahamed <mahafujahamed068@gamil.com>
2023-11-23 22:38:00 +0600

    adds pages

--- src/app/dashboard/add-product/page.js
+++ src/app/dashboard/add-product/page.js
@@ -1,7 +1,9 @@
 const AddProductPage = () => {
     return (
-        <div className="text-center font-serif text-violet-900 text-5xl mb-32">
+        <div className="text-center font-serif text-violet-900 text-5xl mb-32 mt-28">
+            
             Add Product Page for developer Mahafuj.
+
         </div>
     );
 };
--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,4 +1,4 @@
-
+ 
 const DashboardPage = () => {
     return (
         <div>
